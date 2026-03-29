152066377729086279ec393bcf4d401a13744a99
Dennis Park <xcomart@gmail.com>
2026-03-29 09:05:05 +0900

    fx build start

---
+++ src/main/java/comart/tools/jdbgen/JDBGeneratorFX.java
@@ -0,0 +1,16 @@
+package comart.tools.jdbgen;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JDBGeneratorFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
